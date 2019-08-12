e2ab82688f6197b375eb502d0afc00afa8aec91a
CamDHall <camerondylan.h@gmail.com>
2019-08-12 17:10:07 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import JobContainer from './JobContainer';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [jobs, setJobs] = useState(null);
+
+    useEffect(() => {
+        fetch("jobs.json").then((response) => {
+            response.json().then(data => {
+                setJobs(data);
+            })
+        });
+    }, []);
+
+    return (
+        <div className="container">
+            <h1>Cameron</h1>
+            <h1>Hall</h1>
+            <p>Adipiscing diam donec adipiscing tristique risus nec feugiat. Sed enim ut sem viverra aliquet eget sit. Vel risus commodo viverra maecenas accumsan. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Lorem ipsum dolor sit amet consectetur adipiscing. Urna neque viverra justo nec ultrices. Tortor consequat id porta nibh venenatis cras sed felis. Ullamcorper eget nulla facilisi etiam. Augue mauris augue neque gravida in fermentum. Sit amet venenatis urna cursus eget. Elementum curabitur vitae nunc sed velit. Rhoncus urna neque viverra justo nec ultrices dui sapien.</p>
+            {jobs ? jobs.map((job) => {
+                return <JobContainer key={job.title} job={job} />
+            }) : null}
+        </div>
+    );
 }
 
 export default App;
