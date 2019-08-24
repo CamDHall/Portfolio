e0c6ffb7c036cb5bd616eccb6ba4cedbb6858ed6
CamDHall <camerondylan.h@gmail.com>
2019-08-24 12:11:38 -0500

    Add Basic layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import JobContainer from './JobContainer';
-import './App.css';
+import './styles/App.scss';
 
 function App() {
     const [jobs, setJobs] = useState(null);
@@ -14,13 +14,23 @@ function App() {
     }, []);
 
     return (
-        <div className="container">
-            <h1>Cameron</h1>
-            <h1>Hall</h1>
-            <p>Adipiscing diam donec adipiscing tristique risus nec feugiat. Sed enim ut sem viverra aliquet eget sit. Vel risus commodo viverra maecenas accumsan. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Lorem ipsum dolor sit amet consectetur adipiscing. Urna neque viverra justo nec ultrices. Tortor consequat id porta nibh venenatis cras sed felis. Ullamcorper eget nulla facilisi etiam. Augue mauris augue neque gravida in fermentum. Sit amet venenatis urna cursus eget. Elementum curabitur vitae nunc sed velit. Rhoncus urna neque viverra justo nec ultrices dui sapien.</p>
-            {jobs ? jobs.map((job) => {
-                return <JobContainer key={job.title} job={job} />
-            }) : null}
+        <div className="wrapper">
+            <div id="menu">
+                <button>Web</button>
+                <button>Games</button>
+                <button>Education</button>
+                <button>Social</button>
+                <button>Contact</button>
+            </div>
+            <div className="container">
+                <div className="intro">
+                    <h1>Cameron<br />Hall</h1>
+                    <p>Adipiscing diam donec adipiscing tristique risus nec feugiat. Sed enim ut sem viverra aliquet eget sit. Vel risus commodo viverra maecenas accumsan. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Lorem ipsum dolor sit amet consectetur adipiscing. Urna neque viverra justo nec ultrices. Tortor consequat id porta nibh venenatis cras sed felis. Ullamcorper eget nulla facilisi etiam. Augue mauris augue neque gravida in fermentum. Sit amet venenatis urna cursus eget. Elementum curabitur vitae nunc sed velit. Rhoncus urna neque viverra justo nec ultrices dui sapien.</p>
+                </div>
+                {jobs ? jobs.map((job) => {
+                    return <JobContainer key={job.title} job={job} />
+                }) : null}
+            </div>
         </div>
     );
 }
--- src/JobContainer.js
+++ src/JobContainer.js
@@ -1,7 +1,60 @@
 import React, { useState, useEffect } from 'react';
+import './styles/JobContainer.scss';
 
-export default function JobContainer() {
+export default function JobContainer(props) {
+    const [activeProject, setActiveProject] = useState( props.job.projects[0]);
+    const [projects, setProjects]           = useState(props.job.projects);
+
+
+    const changeActiveProject = (projectTitle) => {
+        const element = document.getElementById(`fade-${props.job.company}`);
+
+        const indexOfProject = projects.find(function(item, i) {
+            return item.title === projectTitle;
+        });
+
+        element.classList.remove("fade");
+        void element.offsetWidth;
+        element.classList.add("fade");
+        setTimeout(function() { setActiveProject(indexOfProject) }, 500);
+    }
+
     return(
-        <p>JobContainer</p>
+        <div className="job-container">
+            <img src={"image/" + props.job.imageSrc} alt={props.job.title} />
+            <div className="text-container">
+                <h3>{props.job.company}</h3>
+                <h5>{props.job.title}</h5>
+
+                <div id={`fade-${props.job.company}`}>
+                    <p className={activeProject.title}>{activeProject.description}</p>
+
+                    <h4>Tools:</h4>
+                    <ul className="tools">
+                        {activeProject.tools.map((tool, index) => {
+                            const txt = index < activeProject.tools.length - 1 ? 
+                                `${tool},`: tool;
+                            return <li key={activeProject.title + tool}>{txt}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className="project-links">
+                    {
+                        props.job.projects.map(project => {
+                            var cName = "inactive";
+                            if(project.title === activeProject.title) cName = "active"
+                            return (
+                                <button className={cName} key={project.title} onClick={() => changeActiveProject(project.title)}>
+                                    {project.title.toUpperCase()}
+                                </button>
+                            )
+                        })
+                    }
+                </div>
+                <div className="icon-container">
+                    <div className={props.job.industry}></div>
+                </div>
+            </div>
+        </div>
     )
 }
